289e82373b9f6b162459e5d0621be98ccf2a7bd3
Sebastian A <jsebaslozano2006@gmail.com>
2026-03-14 18:27:47 -0500

    first commit

--- app/main.py
+++ app/main.py
@@ -48,7 +48,7 @@ app = FastAPI(title=settings.project_name)
 # ---------- CORS ----------
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.cors_origins.split(","),
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
